17e25c37c4caf1f0c528ebb089f9d627863af2da
Your Name <your@example.com>
2022-02-17 17:50:58 +1100

    Init Commit

---
+++ part1.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+"""
+part1.py
+
+UNSW COMP9444 Neural Networks and Deep Learning
+
+ONLY COMPLETE METHODS AND CLASSES MARKED "TODO".
+
+DO NOT MODIFY IMPORTS. DO NOT ADD EXTRA FUNCTIONS.
+DO NOT MODIFY EXISTING FUNCTION SIGNATURES.
+DO NOT IMPORT ADDITIONAL LIBRARIES.
+DOING SO MAY CAUSE YOUR CODE TO FAIL AUTOMATED TESTING.
+"""
+
+import torch
+import torch.nn.functional as F
+
+class rnn(torch.nn.Module):
+
+    def __init__(self):
+        super(rnn, self).__init__()
+
+        self.ih = torch.nn.Linear(64, 128)
+        self.hh = torch.nn.Linear(128, 128)
+
+    def rnnCell(self, input, hidden):
+
+        x1= self.ih(input)
+        x2 = self.hh(hidden)
+
+        y = torch.tanh(x1 +x2)
+
+        return y
+
+        """
+        TODO: Using only the above defined linear layers and a tanh
+              activation, create an Elman RNN cell.  You do not need
+              to include an output layer.  The network should take
+              some input (inputDim = 64) and the current hidden state
+              (hiddenDim = 128), and return the new hidden state.
+        """
+
+
+    def forward(self, input):
+        hidden = torch.zeros(128)
+
+        for i in range(input.shape[0]):
+            hidden = self.rnnCell(input[i], hidden)
+
+        return hidden
+        """
+        TODO: Using self.rnnCell, create a model that takes as input
+              a sequence of size [seqLength, batchSize, inputDim]
+              and passes each input through the rnn sequentially,
+              updating the (initally zero) hidden state.
+              Return the final hidden state after the
+              last input in the sequence has been processed.
+        """
+
+class rnnSimplified(torch.nn.Module):
+
+    def __init__(self):
+        super(rnnSimplified, self).__init__()
+        """
+        
+        TODO: Define self.net using a single PyTorch module such that
+              the network defined by this class is equivalent to the
+              one defined in class "rnn".
+        """
+        self.net = torch.nn.RNN(input_size=64, hidden_size =128)
+
+    def forward(self, input):
+        _, hidden = self.net(input)
+
+        return hidden
+
+def lstm(input, hiddenSize):
+    """
+    TODO: Let variable lstm be an instance of torch.nn.LSTM.
+        Variable input is of size [batchSize, seqLength, inputDim]
+    """
+    lstm = torch.nn.LSTM(input_size=input.shape[2],hidden_size=hiddenSize, batch_first=True)
+
+    return lstm(input)
+
+def conv(input, weight):
+    """
+    TODO: Return the convolution of input and weight tensors,
+        where input contains sequential data.
+        The convolution should be along the sequence axis.
+        input is of size [batchSize, inputDim, seqLength]
+    """
+    return F.conv1d(input,weight)
+
